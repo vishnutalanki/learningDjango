90ed3cd5ffba16d4dcd51723d6f316aeef7feeda
vishnutalanki <vishnutalanki77@gmail.com>
2018-04-24 17:13:18 +0530

    hello.py
    Added the first django file - hello.py

---
+++ hello.py
@@ -0,0 +1,21 @@
+import sys
+from django.conf import settings 
+from django.conf.urls import patterns
+from django.http import HttpResponse
+from django.core.management import execute_from_command_line
+ 
+settings.configure(
+    DEBUG=True,
+    SECRET_KEY='asecretkey',
+    ROOT_URLCONF=sys.modules[__name__],
+)
+ 
+def index(request):
+    return HttpResponse('Hello, World')
+ 
+urlpatterns = patterns('',
+    (r'^hello/$', index),
+)
+ 
+if __name__ == "__main__":
+    execute_from_command_line(sys.argv)
