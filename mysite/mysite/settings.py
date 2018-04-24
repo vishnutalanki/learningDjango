9ee15b8c5373bbdcd72be6166b7f5328c6f9acd6
vishnutalanki <vishnutalanki77@gmail.com>
2018-04-24 22:03:33 +0530

    Front end changes
    Front end added

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -82,3 +82,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#print "base dir path", BASE_DIR
+#print os.path.join(os.path.dirname(BASE_DIR), "mysite", "static",  "templates")
+ 
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR), "mysite", "static",  "templates"),
+    #'/home/frankbox/python/djangoapp/mysite/static/templates',
+)
